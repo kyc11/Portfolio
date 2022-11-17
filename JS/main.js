c9662ab64cc9e5302178f700471722b51b6a82b3
김영채 <dudco7787@gmail.com>
2022-11-17 20:58:05 +0900

    2

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,39 @@
 // 포폴 main.js 
 
-$(()=>{
-    
-    // const lbtn = $(".lbtn");
+window.addEventListener("DOMContentLoaded",loadFn);
 
-    $(".lbtn").click(function(){
-        console.log("ddd")
-    })
+function loadFn(){
+
+    console.log("로딩완료!")
+
+    /* 왼쪽버튼 */
+    const lbtn = document.querySelector(".lbtn")
+
+    /* 오른쪽버튼 */
+    const rbtn = document.querySelector(".rbtn")
+
+    /* slide */
+    const slide = document.querySelector("#slide")
+
+    lbtn.onclick = () => {
+        lbc();
+    };
+
+    function lbc(){
+        console.log("왼쪽버튼")
+
+        slide.style.left = "-100%"
+        slide.style.transition = "1s"
+
+        setTimeout(()=>{
+            slide.style.left = "0"
+            slide.style.transition = "none"
+        },1000)
+    };
 
 
 
-}); /////////////////////////
+
+
+}
+
