5e399ecb95898736fbc865f69efc9878e43e9e47
김영채 <dudco7787@gmail.com>
2022-11-28 22:11:55 +0900

    2

--- JS/main3.js
+++ JS/main3.js
@@ -5,24 +5,35 @@ window.addEventListener("DOMContentLoaded",loadFn3);
 function loadFn3(){
     // console.log("로딩완료 3!")
 
-    // 5메인 왼쪽버튼
-    const lbtn2 = document.querySelector(".all img")
+    const abab = document.querySelectorAll(".abab")
 
-    // 5메인 왼쪽버튼
-    const rbtn2 = document.querySelector(".alr img")
+    // 5메인 전체박스
+    const mibx = document.querySelector(".mibx")
 
+    // console.log(abx)
+    
+    abab[0].onclick = () => goSlide(0)
+    abab[1].onclick = () => goSlide(1)
+    
+    function goSlide(dir){
+        console.log("dodo",dir)
+        
+        // 5메인 모든 div
+        let abx = mibx.querySelectorAll("div")
+        
+        if(dir){
+            mibx.appendChild(abx[0]);
+        }
+        else{
+            mibx.insertBefore(abx[abx.length - 1],abx[0])
+        }
+    }
     
 
-    lbtn2.onclick = () => {
-        console.log("왼버2")
-        lbc2()
-    }
-
-    rbtn2.onclick = () => {
-        console.log("오버2")
-    }
-
-    function lbc2(){
         
-    }
+    
+
+    
+    
+
 }
