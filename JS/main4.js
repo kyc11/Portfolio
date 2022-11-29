60bc23ea050f089201c67612e54c7a8245ded0aa
김영채 <larmi9865@gmail.com>
2022-11-29 16:56:57 +0900

    2

--- JS/main4.js
+++ JS/main4.js
@@ -17,45 +17,16 @@ function loadFn4(){
     // 나오는 곳 2
     const sfs2 = document.querySelector(".sfoss2")
 
-    let nnum = 0
-
-    
-    function pffo(){
-        if(nnum===0){
-            psbt.onclick = () => {
-                sfs.classList.add("onn");
-                nnum = 1
-                console.log(nnum)
-            }
-        }
-        console.log(nnum)
-    }
-    
     
 
-    function pffo2(){
-        if(nnum===1){
-            psbt.onclick = () => {
-                sfs.classList.remove("onn");
-                nnum = 0
-                console.log(nnum)
-            }
-        }
-    }
     
-    pffo();
-
-    pffo2();
+    
 
     
     
 
     
 
-    psbt2.onclick = () => {
-        // console.log("dqwdqw2")
-        sfs2.classList.add("onn2")
-
-    }
+    
     
 }
