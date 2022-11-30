7b1569eca6acb34f7ab6508fc64ee628eb7382ae
김영채 <larmi9865@gmail.com>
2022-11-30 17:02:54 +0900

    2

--- JS/main2.js
+++ JS/main2.js
@@ -7,30 +7,15 @@ function loadFn2(){
     // console.log("로딩완료!2");
 
     /* #slide2 li f */
-    const l1 = document.querySelector("#slide2 li:first-child");
+    const l1 = document.querySelector(".b2g1");
 
     /* #slide2 li s */
-    const l2 = document.querySelector("#slide2 li:nth-child(2)");
+    const l2 = document.querySelector(".b2g2");
 
     /* #slide2 li t */
-    const l3 = document.querySelector("#slide2 li:last-child");
-
-    function slide2auto(){
-        l1.style.opacity = 1;
-        setTimeout(() => {
-            l1.style.opacity = 0;
-            l2.style.opacity = 1;
-        }, 3000);
-        setTimeout(() => {
-            l2.style.opacity = 0;
-            l3.style.opacity = 1;
-        }, 6000);
-        setTimeout(() => {
-            l3.style.opacity = 0;
-        }, 9000);
-    };
+    const l3 = document.querySelector(".b2g3");
     
-     ! function slide2auto(){
+        !function slide2auto(){
         l1.style.opacity = 1;
         setTimeout(() => {
             l1.style.opacity = 0;
